eaed25f27605e50384072c15b8fd6cbf9ae0c819
Tecco2233 <biel.tecco@gmail.com>
2019-07-07 02:13:41 -0300

    aaaa

---
+++ Testandocoisa/src/testandocoisa/Testandocoisa.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testandocoisa;
+
+import java.util.*;
+
+/**
+ *
+ * @author house
+ */
+public class Testandocoisa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner aa = new Scanner(System.in);
+        System.out.println("digita um numero ai fazendo favor");
+    int a = aa.nextInt();
+    do {
+        System.out.println(a);
+        a++;
+    } while (a <= 23);
+        System.out.println("Digita outro");
+        int b = aa.nextInt();
+        
+        while (b <= 23){
+            System.out.println(b);
+            b++;
+        }
+    }
+
+}
+    
+    
+    
+    
+    
+
